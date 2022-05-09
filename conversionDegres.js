33e16b8ad20cfd298dd5a9d26e766a18fa69e794
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-09 16:41:00 +0200

    CHANGE correction premiere partie

--- conversionDegres.js
+++ conversionDegres.js
@@ -1,17 +1,16 @@
 // exercice 1let tableauNombreAleaTire = [];
 let compteur = 0;
+tableauNombreAleaTire = [100];
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 do {
     let nombreAlea = getRandomArbitrary(0,100);
-    // si le nombre n'a pas déjà été tiré    if(!tableauNombreAleaTire.includes(nombreAlea)){
+    // si le nombre n'a pas déjà été tiré    
+    if(!tableauNombreAleaTire.includes(nombreAlea)){
         tableauNombreAleaTire.push(nombreAlea);
-    }
-
-    compteur++;
+     } compteur++;
 } while(tableauNombreAleaTire.length < 100);
-
 console.log("On a fait " + compteur + " tirages");
 console.log(tableauNombreAleaTire);

ac284af02cd2396d43dfb4ac1b26725bff784d36
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-09 12:09:03 +0200

    ADD : Proposition solution no IHM

---
+++ conversionDegres.js
@@ -0,0 +1,8 @@
+function conversionCtoF (degreCelsius){
+    const Farhenheit = 32;
+    console.log("Votre température de " +degreCelsius +" équivaut à " +(degreCelsius + Farhenheit) 
+    +" degrés Farhenheit");
+}
+
+conversionCtoF(0);
+conversionCtoF(32);

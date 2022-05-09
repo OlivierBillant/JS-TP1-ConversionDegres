87287f0ee3e3f90f93f1035cce6663317f38e6d0
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-09 16:53:38 +0200

    ADD : ajout exercice 2

--- conversionDegres.js
+++ conversionDegres.js
@@ -1,6 +1,6 @@
-// exercice 1let tableauNombreAleaTire = [];
+// exercice 1
+let tableauNombreAleaTire = [];
 let compteur = 0;
-tableauNombreAleaTire = [100];
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
@@ -14,3 +14,9 @@ do {
 } while(tableauNombreAleaTire.length < 100);
 console.log("On a fait " + compteur + " tirages");
 console.log(tableauNombreAleaTire);
+
+// exercice 2
+let phrase = "Une chaine avec des lettres dans un certain ordre pour donner du sens";
+let phraseSansEspaces = phrase.replace(/\s/g,'');
+let tableau = Array.from(phraseSansEspaces.toLowerCase());
+console.log(tableau.sort());
